86c192bf14d8c99c332ccd073b1f80ead5609d74
Kyle Moffett <kyle.moffett@hotmail.com>
2016-10-18 08:23:53 -0700

    change

--- TestEnvironment/TestEnvironment/Source1.cpp
+++ TestEnvironment/TestEnvironment/Source1.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+
+
 int main()
 {
-	cout << "testing..." << endl;
+	string str1 = "Chi";
+	string str2 = "Beta";
+	int result;
+	result = str1.compare(str2);
+	cout << result << endl;
+	
 	system("pause");
 	return 0;
 }
