e33cf506b002b54e7ef6705bfc8557b80fb14c6c
Kyle Moffett <kyle.moffett@hotmail.com>
2018-01-09 15:39:59 -0700

    testing datetime class

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 
-print ("This is the main file")
+from datetime import *
+
+class Payout():
+    def __init__(self, hour, minute):
+        self.payTime = time(hour, minute)
+    users = []
+
+myPayout = Payout(17, 30)
+#print ("Payout time: " + myPayout.payTime.isoformat())
+
+print ("Payout time: " + str(myPayout.payTime))
+
+currentTime = datetime.now()
+
+delta_hours = myPayout.payTime.hour - currentTime.hour
+delta_minutes = myPayout.payTime.minute - currentTime.minute + 60
+
+print ("Time until payout: " + str(delta_hours) + ":" + str(delta_minutes))
